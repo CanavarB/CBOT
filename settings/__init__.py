c19500642ff350278cad8cfecd4f9f331d28a6ba
CB <tokurekin8@gmail.com>
2023-03-20 06:31:08 +0300

    Origin fix

---
+++ settings/__init__.py
@@ -0,0 +1,25 @@
+from termcolor import colored
+
+from CDB import CDB, MemberNotFoundError, UserNotFoundError, ColumnValueError, GuildLectureNotFoundError
+from CMail import CMail
+from CLogger import CLogger
+
+
+##############################
+                            ##
+DEBUG = True                ##
+                            ##
+##############################
+
+if DEBUG:
+    print(colored("Development Mode", 'green'))
+    from settings.development import *
+else:
+    print(colored("Production Mode", 'red'))
+    from settings.production import *
+
+
+#CONSTs
+LOGGER = CLogger(PATH = LOG_PATH, LOGLEVEL=LOG_LEVEL)
+DB = CDB(SEMESTER_DB_PATH, GUILD_DB_PATH)
+MAIL = CMail(MAILADD=CBOT_MAIL_ADD, MAILADD_PASS=CBOT_MAIL_PASS)
